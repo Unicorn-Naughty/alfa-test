4dc44854b6205af169182ee84ab38393e78ae5b2
Max <gccccvbnnnn@mail.ru>
2024-12-08 19:19:17 +0700

    fixxx

--- app/products/product/[id]/page.tsx
+++ app/products/product/[id]/page.tsx
@@ -1,12 +1,13 @@
-
+import { ProductCard } from "@/app/components/shared/product-card";
+import { api } from "@/services/api";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const Page = async ({ params }: {params : any}) => {
   const { id } = await params;
- 
+  const product = await api.product.fetchProduct(id);
   return (
     <div>
-      {id}
+      <ProductCard item={product} />
     </div>
   );
 };
